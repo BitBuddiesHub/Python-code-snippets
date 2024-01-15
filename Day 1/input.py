11502c784c875c37199452c5bd86481cbea0a8ad
“Last-emo-boygit config --global user.email chenyuqian0122@outlook.com” <chenyuqian0122@outlook.com>
2024-01-15 15:57:38 +0800

    Update Day 1

--- Day 1/input.py
+++ Day 1/input.py
@@ -1,2 +1,35 @@
 Name = input("请输入你的名字:")
+Age = int(input("请输入你的年龄:"))
 print("你好," + Name)
+
+print("我的名字是{Name}")
+print(f"我的名字是{Name}")
+
+print("我的名字是 %s 并且我的年龄是 %i 岁" % (Name, Age))
+
+
+height = 175.5
+
+print("My height is %.2f cm." % height)
+
+number = 123456.789
+
+print("Scientific notation: %e" % number)
+
+number = 255
+
+print("Hexadecimal: %x" % number)
+print("Octal: %o" % number)
+
+char = 'A'
+percentage = 85.5
+
+print("Character: %c" % char)
+print("You scored %f%% on your test." % percentage)
+
+width = 10
+number = 3.14159
+
+print("Right aligned: [%10s]" % ("right",))
+print("Left aligned: [%-10s]" % ("left",))
+print("Number with fixed width: [%*.*f]" % (width, 3, number))
